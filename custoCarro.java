f5a775fba40558bed4aad75d65df9b62506025c3
miqueline-melo <miquelinepereiramelo@gmail.com>
2023-05-24 18:51:44 -0300

    atualizacao

--- custoCarro.java
+++ custoCarro.java
@@ -1,2 +1,24 @@
-package treino;public class custoCarro {
+package treino;
+
+import java.util.Scanner;
+
+public class custoCarro {
+    public static void main(String[] args) {
+
+        double valorCarro, valorFinal, percentualDistribuidor, imposto;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Digite o valor do veiculo: ");
+        valorCarro=teclado.nextDouble();
+
+        percentualDistribuidor = 0.28;
+        imposto = 0.45;
+
+        valorFinal = valorCarro + valorCarro*percentualDistribuidor + valorCarro*imposto;
+
+        System.out.println("Valor total do veiculo é: " + valorFinal);
+
+        teclado.close();
+    }
 }
