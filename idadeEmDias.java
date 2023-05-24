9331e2f2afa8073a481e2ee86e59ffc1d2ec4479
miqueline-melo <miquelinepereiramelo@gmail.com>
2023-05-24 18:50:55 -0300

    atualizacao

---
+++ idadeEmDias.java
@@ -0,0 +1,25 @@
+package treino;
+
+import java.util.Scanner;
+
+public class idadeEmDias {
+    public static void main(String[] args) {
+
+        int dia=0, mes=0, ano=0, idade;
+
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o dia de nascimento: ");
+        dia = teclado.nextInt();
+        System.out.println("Digite o mes de nascimento: ");
+        mes = teclado.nextInt();
+        System.out.println("Digite o ano de nascimento: ");
+        ano = teclado.nextInt();
+
+        idade = dia + mes*30 + ano*365;
+
+        System.out.println("A sua idade em dias é: " +  idade);
+
+
+    }
+}
