41539aa4f6cb9e29c21c1b9dec53b85448c8484d
miqueline-melo <miquelinepereiramelo@gmail.com>
2023-05-29 17:35:51 -0300

    atualizacao

--- saldo.java
+++ saldo.java
@@ -1,2 +1,15 @@
-package treino;public class saldo {
+package treino;
+
+public class saldo {
+    public static void main(String[] args) {
+        double saldo, acrescimo, total;
+
+        saldo = 5000;
+        acrescimo= 0.01;
+
+        total = saldo+(saldo*acrescimo);
+
+        System.out.println("Valor total do saldo: " + total);
+
+    }
 }
