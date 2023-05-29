82f3f808bf1f8699da059d73f93157ae7351e0b4
miqueline-melo <miquelinepereiramelo@gmail.com>
2023-05-29 18:14:27 -0300

    atualizacao

--- salarioMinimo.java
+++ salarioMinimo.java
@@ -1,2 +1,22 @@
-package treino;public class salarioMinimo {
+package treino;
+
+import java.util.Scanner;
+
+public class salarioMinimo {
+    public static void main(String[] args) {
+        double salMinimo, salUsuario, quantSalMinimo;
+
+        salMinimo = 788.00;
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o valor do seu salario: ");
+        salUsuario = teclado.nextDouble();
+
+        quantSalMinimo = salUsuario/salMinimo;
+
+        System.out.println("O seu salario e equivalente a " + quantSalMinimo + " salarios minino");
+
+
+
+    }
 }
